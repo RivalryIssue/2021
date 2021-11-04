889f59fb9741a002878d93dfa7d2e78d2c7da1de
Rimaz Khan <rimazk@umich.edu>
2021-11-03 20:51:21 -0400

    basic progress bar

--- src/js/components/DonationGraphic.jsx
+++ src/js/components/DonationGraphic.jsx
@@ -6,6 +6,15 @@ export default function DonationGraphic() {
     daily: "",
   });
 
+  const updateProgressBar = (value) => {
+    console.log(value);
+    let progressbar = document.getElementById("download");
+    value = Math.min(value, 100);
+    if (value >= 0 && value <= 100) {
+      progressbar.style.width = value + "%";
+    }
+  };
+
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/RivalryIssue/donations-scraper/master/data.json"
@@ -14,11 +23,13 @@ export default function DonationGraphic() {
       .then((result) => {
         setMoney(result.data);
       });
+    setTimeout(() => updateProgressBar(70), 2000);
   }, []);
+
   return (
     <div className="graphic-container">
       <div class="progress">
-        <div class="progress__bar"></div>
+        <div id="download" className="progress__bar"></div>
       </div>
     </div>
   );
