2dc9e5300fddb37821bfa3563d0674d20ec07523
Rimaz Khan <rimazk@umich.edu>
2021-11-14 11:52:49 -0500

    improve responsiveness

--- src/js/components/DonationGraphic.jsx
+++ src/js/components/DonationGraphic.jsx
@@ -6,11 +6,11 @@ export default function DonationGraphic() {
   const [lanternMoney, setLanternMoney] = useState("");
   const [dailyMoney, setDailyMoney] = useState("");
 
-  const updateProgressBar = (value) => {
+  const updateProgressBar = (value, elt) => {
     let str_array = value.slice(1).split(",");
     let str = str_array[0] + str_array[1];
     value = Number(str) / 100;
-    let progressbar = document.getElementById("download");
+    let progressbar = document.getElementById(elt);
     value = Math.min(value, 100);
     if (value >= 0 && value <= 100) {
       progressbar.style.width = value + "%";
@@ -24,11 +24,13 @@ export default function DonationGraphic() {
       .then((response) => response.json())
       .then((result) => {
         setLanternMoney(result.data.lantern);
+        setDailyMoney(result.data.daily);
       });
   }, []);
 
   useEffect(() => {
-    updateProgressBar(lanternMoney);
+    updateProgressBar(lanternMoney, "lanternProgress");
+    updateProgressBar(dailyMoney, "dailyProgress");
   }, [lanternMoney]);
 
   return (
@@ -38,22 +40,26 @@ export default function DonationGraphic() {
         <h2 className="donation_title">The Michigan Daily</h2>
         <div className="progess_container">
           <div className="progress">
-            <div id="download" className="progress__bar"></div>
+            <div
+              id="dailyProgress"
+              className="progress__bar"
+              style={{ backgroundColor: "#374567" }}
+            ></div>
           </div>
-          <h4 className="donation_amount">{lanternMoney} of $10,000</h4>
+          <h4 className="donation_amount">{dailyMoney} of $10,000</h4>
         </div>
       </div>
       <div className="donation_container">
         <h2 className="donation_title">The Lantern</h2>
         <div className="progess_container">
           <div className="progress">
-            <div id="download" className="progress__bar"></div>
+            <div id="lanternProgress" className="progress__bar"></div>
           </div>
           <h4 className="donation_amount">{lanternMoney} of $10,000</h4>
         </div>
       </div>
 
-      <div className="buttons-container">
+      {/* <div className="buttons-container">
         <div className="donation-button">
           <a
             href="https://give.communityfunded.com/o/university-of-michigan/i/giving-tuesday/s/tmd-lantern-rivalry"
@@ -67,7 +73,7 @@ export default function DonationGraphic() {
             <img src={lantern} />
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
